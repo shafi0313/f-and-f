e470de543658ced070ef668379f0c34e8fcdd6aa
shafi1997 <msh.shafiul@gmail.com>
2024-01-20 07:26:42 +0600

    Change footer

--- resources/views/admin/layouts/includes/footer.blade.php
+++ resources/views/admin/layouts/includes/footer.blade.php
@@ -2,13 +2,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                <script>document.write(new Date().getFullYear())</script> © Jidox - Coderthemes.com
+                {{ date('Y') }} &copy; <a href="{{ URL::to('https://softgiantbd.com/') }}" target="_blank">Soft Giant BD</a>. All Rights Reserved.
             </div>
             <div class="col-md-6">
                 <div class="text-md-end footer-links d-none d-md-block">
-                    <a href="javascript: void(0);">About</a>
-                    <a href="javascript: void(0);">Support</a>
-                    <a href="javascript: void(0);">Contact Us</a>
+                    <a href="{{ URL::to('https://softgiantbd.com/about') }}" target="_blank">About</a>
+                    {{-- <a href="javascript: void(0);">Support</a> --}}
+                    <a href="{{ URL::to('https://softgiantbd.com/contact') }}" target="_blank">Contact Us</a>
                 </div>
             </div>
         </div>
