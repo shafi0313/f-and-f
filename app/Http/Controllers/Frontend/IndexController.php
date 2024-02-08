c7df81203c4efc087cd6ee683f0e9efcf7893d6f
shafi1997 <msh.shafiul@gmail.com>
2024-02-08 08:40:30 +0600

    Add Property model to IndexController

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Frontend;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Property;
 
 class IndexController extends Controller
 {
     public function index()
     {
         $data['sliders'] = Slider::where('is_active', 1)->get();
+        $data['properties'] = Property::where('is_active', 1)->get();
         return view('frontend.index', $data);
     }
 }
