4b31fff6b6f5900a0febe5ebdf5585face21ac81
shafi1997 <msh.shafiul@gmail.com>
2024-02-12 07:56:32 +0600

    Add AboutSeeder to DatabaseSeeder

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\About;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AboutSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class AboutSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        About::create([
+            'content' => '<p><img style="width: 1100px;" data-filename="1484821634.png" src="/uploads/images/about/df3e21fa210.webp"><p>We are running our business competitively with good reputation in WA last ten years. F and F Property Maintenance is a sister concern of WACabs Pty Ltd, F and F Accounting, Focus IT Solution, accounting software provider AARKS.</p><p>We are proud to be WA owned and operated company and want to serve our community with good faith and reputation.</p></p>',
+        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([UserSeeder::class]);
         $this->call([PropertySeeder::class]);
         $this->call([SliderSeeder::class]);
+        $this->call([AboutSeeder::class]);
 
     }
 }
